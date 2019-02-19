6fc4b0f2a7aeea2448905a9e60dfcd4ee30dff4c
3mpirical <3mpirical10510@gmail.com>
2019-02-19 16:37:03 -0700

    built basic crud features; index, show, new, and edit, are functional

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,47 @@
+class PeopleController < ApplicationController
+  def index
+    @people = Person.all()
+  end
+
+  def show
+    @person = Person.find(params[:id])
+  end
+
+  def new
+    @person = Person.new()
+  end
+
+  def create
+    @person = Person.new(person_params())
+
+    if(@person.save())
+      redirect_to(person_path)
+    else
+      render(:new)
+    end
+  end
+
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:id])
+
+    if( @person.update(person_params()) )
+      redirect_to(person_path)
+    else
+      render(:edit)
+    end
+  end
+
+  def destroy
+    Person.destroy(params[:id])
+    redirect_to(people_path)
+  end
+
+  private
+    def person_params
+      return params.require(:person).permit(:name, :age, :hair_color, :eye_color, :gender, :alive)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+Person.destroy_all()
+
+for i in (0..19)
+    Person.create(
+        name: Faker::TvShows::RuPaul.queen(),
+        age: Faker::Number.number(2),
+        hair_color: Faker::Color.color_name(),
+        eye_color: Faker::Color.color_name(),
+        gender: Faker::Gender.type(),
+        alive: Faker::Boolean.boolean() ,
+    )
+end
+puts "__________ QUEENS CREATED __________"
+Person.all().each_with_index() {|queen, index|
+    puts "_____Queen: #{index}_____"
+    puts "Name: #{queen.name}"
+    puts "Age: #{queen.age}"
+    puts "Hair Color: #{queen.hair_color}"
+    puts "Eye Color: #{queen.eye_color}"
+    puts "Gender: #{queen.gender}"
+    puts "Alive: #{queen.alive}"
+}
